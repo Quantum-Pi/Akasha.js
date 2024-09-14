dbe73b464c31151b277d7464fe8325ac127741dc
Quantum-Pi <quantumpie8@gmail.com>
2024-09-14 11:30:15 -0500

    feat: exposed additional API routes

--- src/Akasha.ts
+++ src/Akasha.ts
@@ -3,6 +3,15 @@ import { operations } from './schema'
 type GetCalculationsForUser =
     operations['getApiGetCalculationsForUserById']['responses']['200']['content']['application/json']
 
+type GetApiFiltersArtifacts =
+    operations['getApiFiltersArtifacts']['responses']['200']['content']['application/json']
+
+type GetApiFiltersCategories =
+    operations['getApiFiltersCategories']['responses']['200']['content']['application/json']
+
+type GetApiFiltersCharacters =
+    operations['getApiFiltersCharacters']['responses']['200']['content']['application/json']
+
 export default class AkashaAPI {
     private baseURL = 'https://akasha.cv/api/'
     private uuid: string
@@ -13,6 +22,24 @@ export default class AkashaAPI {
 
     async getCalculationsForUser(): Promise<GetCalculationsForUser> {
         const url = `${this.baseURL}getCalculationsForUser/${this.uuid}`
+        const response = await fetch(url, { method: 'GET' })
+        return await response.json()
+    }
+
+    async getApiFiltersArtifacts(): Promise<GetApiFiltersArtifacts> {
+        const url = `${this.baseURL}filters/artifacts`
+        const response = await fetch(url, { method: 'GET' })
+        return await response.json()
+    }
+
+    async getApiFiltersCategories(): Promise<GetApiFiltersCategories> {
+        const url = `${this.baseURL}filters/categories`
+        const response = await fetch(url, { method: 'GET' })
+        return await response.json()
+    }
+
+    async getApiFiltersCharacters(): Promise<GetApiFiltersCharacters> {
+        const url = `${this.baseURL}filters/characters`
         const response = await fetch(url, { method: 'GET' })
         return await response.json()
     }
